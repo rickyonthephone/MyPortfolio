29ba1388898405175916431800fa53254d0db1c6
Richard Crippen <richard.crippen@gmail.com>
2021-08-13 16:56:25 -0400

    styling updates

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import {Switch, BrowserRouter as Router, Link, Route} from "react-router-dom";
 import MyNavbar from './components/Navbar/MyNavbar';
 import AboutMe from './pages/Landing/AboutMe';
 import Footer from './components/Footer/Footer';
-
 import './App.css';
 
 function App() {
@@ -34,9 +33,11 @@ function App() {
           <AboutMe />
         </Route>
 
-        <Footer />
       </div>
     </Router>
+    <div className='bottomNav'>
+      <Footer />
+    </div>
   </div>
   );
 }
